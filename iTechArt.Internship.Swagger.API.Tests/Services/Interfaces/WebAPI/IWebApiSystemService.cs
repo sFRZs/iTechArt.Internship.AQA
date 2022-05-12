469b0f14f0ed5ed7e7c48bca218a744f10f62b57
Timofey <studio28.frz@gmail.com>
2022-05-12 14:27:27 +0300

    Add negative test for WebApi.System and WebApi.Task

--- iTechArt.Internship.Swagger.API.Tests/Services/Interfaces/WebAPI/IWebApiSystemService.cs
+++ iTechArt.Internship.Swagger.API.Tests/Services/Interfaces/WebAPI/IWebApiSystemService.cs
@@ -6,6 +6,8 @@ namespace iTechArt.Internship.Swagger.API.Tests.Services.Interfaces.WebAPI
     public interface IWebApiSystemService
     {
         public Task<IRestResponse<T>> GetAllSystems<T>();
+        public Task<IRestResponse> GetAllSystems();
         public Task<IRestResponse<T>> PostSystem<T>(object body);
+        public Task<IRestResponse> PostSystem(object body);
     }
 }
--- iTechArt.Internship.Swagger.API.Tests/Services/Interfaces/WebAPI/IWebApiTaskService.cs
+++ iTechArt.Internship.Swagger.API.Tests/Services/Interfaces/WebAPI/IWebApiTaskService.cs
@@ -6,6 +6,8 @@ namespace iTechArt.Internship.Swagger.API.Tests.Services.Interfaces.WebAPI
     public interface IWebApiTaskService
     {
         public Task<IRestResponse<T>> GetAllTasksForSystem<T>(string systemId);
+        public Task<IRestResponse> GetAllTasksForSystem(string systemId);
         public Task<IRestResponse<T>> PostTask<T>(object body);
+        public Task<IRestResponse> PostTask(object body);
     }
 }
